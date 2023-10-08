5d63dc9a050dcdbf81627c8e68aef82acc929f7b
JassemBough <jassem.b@devokt.com>
2023-10-08 21:49:58 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Logger as PinoLogger, LoggerErrorInterceptor } from 'nestjs-pino';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const PORT = app.get(ConfigService).get('main.port');
+
+  app.enableCors();
+  app.use(helmet());
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.useLogger(app.get(PinoLogger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  await app.listen(PORT, () => {
+    Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
+  });
+}
+bootstrap();
