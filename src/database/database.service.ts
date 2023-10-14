665a505db7f1744fed87f25da90cf92f73a24de2
macfim <boughattasjassem@outlook.com>
2023-10-14 17:55:11 +0100

    format code

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,10 +1,15 @@
 import type { OnModuleInit } from '@nestjs/common';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect()
+      .then(() => Logger.log('Connected to database', 'PrismaService'))
+      .catch((error: Error) => {
+        Logger.error(error, 'PrismaService');
+        process.exit(1);
+      });
   }
 }
